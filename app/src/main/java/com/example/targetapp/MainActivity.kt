e53ff3fa9f4c5f2df641b7cb6b3faa3661f41ed5
NalanUlusoy <nalan.ulusoy@turkmedya.com.tr>
2019-11-22 17:03:26 +0300

    navigation component
    Android jetpack navigation component implementation

---
+++ app/src/main/java/com/example/targetapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.targetapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.mainNavFragment)
+
+        // Set up ActionBar
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        (this as AppCompatActivity).supportActionBar?.title = getString(R.string.app_name)
+        // Set up navigation menu
+        navigationView.setupWithNavController(navController)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.mainNavFragment), drawerLayout)
+    }
+
+}
